29ac3e74cfe450f0f9345971e642597005168702
Liu min <781775946@qq.com>
2018-05-07 19:49:29 +0800

    第3次提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -18,15 +18,21 @@ Page({
     duration: 1000,
     //进度条
     progress: [10,50,90],
-    progressIndex: 0,//当前信息填写进度
+    progressIndex: 1,//当前信息填写进度
     progressAnimation: false,//进度条动画
     //表单
     broadband: ['这是个宽带', '大王卡'],
     broadbandIndex: 0,
     setMeal: ['这是个套餐','和家庭套餐'],
     setMealIndex : 0,
+    userName: '',
+    userMobile: '',
+    referrerMobile: '',
     //温馨提示
-    tipsText: '温馨提示：宽带套餐政策以当地营业厅现行政策为准！'
+    tipsText: '温馨提示：宽带套餐政策以当地营业厅现行政策为准！',
+    //定位图标
+    locationIcon: assets + 'img/location_icon.png',
+    locationText: '',
   },
   //选择宽带
   pickerbroadband: function (e) {
@@ -40,10 +46,40 @@ Page({
       setMealIndex: e.detail.value
     })
   },
+  //填写姓名
+  inputUserName: function(e){
+    this.setData({
+      userName: e.detail.value
+    })
+  },
+  //填写号码
+  inputUserMobile: function (e) {
+    this.setData({
+      userMobile: e.detail.value
+    })
+  },
+  //填写推荐人号码
+  inputReferrerMobile: function (e) {
+    this.setData({
+      referrerMobile: e.detail.value
+    })
+  },
   //完成第一步
   submitMsg: function(e) {
     this.setData({
       progressIndex: this.data.progressIndex += 1
     });
+    if( this.data.progressIndex == 2 ){
+      wx.getLocation({
+        type: 'wgs84',
+        success: function (res) {
+          var latitude = res.latitude
+          var longitude = res.longitude
+          var speed = res.speed
+          var accuracy = res.accuracy
+          console.log(res);
+        }
+      })
+    }
   } 
 })
